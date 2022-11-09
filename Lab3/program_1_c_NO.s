ef707df1e5245678c53b6a00ffcfc30d914f5bb5
KEVIN CARDINALE S305035 <s305035@studenti.polito.it>
2022-11-09 10:01:23 +0100

    added lab 5

--- Lab3/program_1_c_NO.s
+++ Lab3/program_1_c_NO.s
@@ -37,71 +37,81 @@ v6: .space 480
 v7: .space 480
 
     .text
-main:
-    daddui r1, r0, 0        ;counter
-    daddui r2, r0, 480      ;limit for exit condition
 
-loop:   ;first group of loads
-        l.d f1, v1(r1)
-        l.d f2, v2(r1)
-        l.d f3, v3(r1)
-        add.d f5, f1, f2
-        l.d f4, v4(r1)
-        mul.d f8, f4, f1
-        add.d f9, f2, f3
-        daddui r4, r1, 8
+;r8 contains the intex of the arrays
+;r9 contains the lenght of the arrays
+;f10 contains values of v5
+;f11 contains values of v6
+;f12 contains values of v7
 
-        ;second group of loads
-        l.d f10, v1(r4)
-        l.d f11, v2(r4)
-        l.d f12, v3(r4)
-        l.d f13, v4(r4)
-        mul.d f5, f5, f3
-        add.d f14, f10, f11
-        add.d f18, f11, f12
-        daddui r4, r1, 16
-        add.d f5, f5, f4
+main:   daddi r8,r0,0
+        daddi r9,r0,472
 
-        ;third group of loads
-        l.d f19, v1(r4)
-        l.d f20, v2(r4)
-        mul.d f14, f14, f12
-        s.d f5, v5(r1)
-        l.d f21, v3(r4)
-        l.d f22, v4(r4)
-        div.d f6, f5, f8
-        add.d f23, f19, f20
-        add.d f14, f14, f13
-        add.d f27, f20, f21
-        mul.d f17, f13, f10
-        mul.d f26, f22, f19
-        mul.d f23, f23, f21
+loop:   ;UNROLL 1
+        l.d f1,v1(r8)
+        l.d f2,v2(r8)
+        l.d f3,v3(r8)
+        l.d f4,v4(r8)
 
-        ;store values
+        mul.d f6,f4,f1
+        add.d f5,f1,f2
+        add.d f7,f2,f3
 
-        div.d f15, f14, f17
-        s.d f6, v6(r1)
+        mul.d f5,f5,f3
+        add.d f5,f5,f4
+        div.d f6,f5,f6
+        mul.d f7,f6,f7
 
-        s.d f14, v5(r4)
+        s.d f5,v5(r8)
+        s.d f6,v6(r8)
+        s.d f7,v7(r8)
 
-        mul.d f7, f6, f9
-        s.d f7, v7(r1)
+        daddi r8,r8,8
 
-        add.d f23, f23, f22
-        s.d f23, v5(r4)
+        ;UNROLL 2
 
-        div.d f15, f14, f17
-        s.d f6, v6(r1)
+        l.d f1,v1(r8)
+        l.d f2,v2(r8)
+        l.d f3,v3(r8)
+        l.d f4,v4(r8)
 
-        daddui r1, r1, 24
-        div.d f24, f23, f26
+        mul.d f6,f4,f1
+        add.d f5,f1,f2
+        add.d f7,f2,f3
 
-        mul.d f16, f15, f18
-        s.d f16, v7(r4)
+        mul.d f5,f5,f3
+        add.d f5,f5,f4
+        div.d f6,f5,f6
+        mul.d f7,f6,f7
 
-        s.d f15, v6(r4)
+        s.d f5,v5(r8)
+        s.d f6,v6(r8)
+        s.d f7,v7(r8)
 
-        mul.d f25, f24, f27
-        bne r1, r2, loop
-        s.d f25, v7(r4) ;for branch delay
-        halt
+        daddi r8,r8,8
+
+
+        ;UNROLL 3
+
+        l.d f1,v1(r8)
+        l.d f2,v2(r8)
+        l.d f3,v3(r8)
+        l.d f4,v4(r8)
+
+        mul.d f6,f4,f1
+        add.d f5,f1,f2
+        add.d f7,f2,f3
+
+        mul.d f5,f5,f3
+        add.d f5,f5,f4
+        div.d f6,f5,f6
+        mul.d f7,f6,f7
+
+        s.d f5,v5(r8)
+        s.d f6,v6(r8)
+        s.d f7,v7(r8)
+        
+        bne r8,r9,loop
+        daddi r8,r8,8
+ 
+exit: halt
